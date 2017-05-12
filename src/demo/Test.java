e381845759a61693711d51419223709410a4fdec
Administrator <Administrator@10.6.96.112>
2017-05-12 09:49:46 +0800

    jar add

--- src/demo/Test.java
+++ src/demo/Test.java
@@ -1,8 +1,22 @@
 package demo;
 
+import java.util.Arrays;
+import java.util.List;
+
+import net.hydromatic.linq4j.Linq4j;
+
 public class Test {
 
 	public static void main(String[] args) {
-		
+		List<Integer> iList = Arrays.asList(1,2,3,4,5);
+		Linq4j.asEnumerable(iList).select(it->String.valueOf(it*3)).toList();
+		Linq4j.asEnumerable(iList).select(it->
+		{
+			Integer a = it+1;
+			a+=1;
+			return a;
+		}).toList();
+		int aa =Linq4j.asEnumerable(iList).aggregate(0,(acc,it)->acc+it);
+		System.out.println(aa);
 	}
 }
